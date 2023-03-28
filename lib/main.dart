8dcd5ea8223cc1d80231aecfed2bbc3a95539f38
Yunitrish <yunthomas006@gmail.com>
2023-03-28 22:23:21 +0800

    測試版

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ class LoadPageState extends State<LoadPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: darkTheme ? null : Colors.grey,
+        backgroundColor: darkTheme ? null : Colors.white,
         appBar: AppBar (
           title: const Text("阿里山蔣中正行館"),
           backgroundColor: Colors.white60,
@@ -38,7 +38,7 @@ class LoadPageState extends State<LoadPage> {
         ),
         body: Container (
             color: Colors.black26,
-            child: DraggableImage(imageUrl: "assets/map.png")
+            child: ImageMap()
         ),
         floatingActionButton: FloatingActionButton.extended(
           onPressed: () {
@@ -51,112 +51,129 @@ class LoadPageState extends State<LoadPage> {
   }
 }
 //==============================================================================
-class DraggableImage extends StatefulWidget {
-  final String imageUrl;
-  final double maxScale;
 
-  const DraggableImage({super.key,
-    required this.imageUrl,
-    this.maxScale = 2.0
-  });
-
+class ImageMap extends StatefulWidget {
   @override
-  State<DraggableImage> createState() => _DraggableImageState();
+  _ImageMapState createState() => _ImageMapState();
 }
 
-class _DraggableImageState extends State<DraggableImage> {
-  Offset _tapPosition = const Offset(0, 0);
+class _ImageMapState extends State<ImageMap> {
+  final Map<String, String> _imageMap = {
+    'topLeft': 'assets/east/01.jpg',
+    'topRight': 'assets/east/02.jpg',
+    'bottomLeft': 'assets/east/03.jpg',
+    'bottomRight': 'assets/east/04.jpg',
+  };
 
-  String _room = "none";
-
-  String currentRoomPicture = "none";
-
-  void getPos(TapDownDetails details) {
-    final pos = details.localPosition;
-    var x = pos.dx;
-    var y = pos.dy;
-    if (14 < x && x < 195) {
-      if(75.5 < y && y < 125) {
-        if (x < 30) {
-          _room = "洋館14";
-          currentRoomPicture = "west/13~15";
-        }
-        else if (x < 73.5) {
-          _room = "洋館05";
-          currentRoomPicture = "west/05";
-        }
-        else if(73.5 < x && x < 133.5) {
-          _room = "洋館04";
-          currentRoomPicture = "west/04";
-        }
-        else if(133.5 < x && x < 213.5) {
-          _room = "洋館03";
-          currentRoomPicture = "west/03";
-        }
-      }
-      else if (150 < y && y < 190) {
-        if (x < 30) {
-          _room = "洋館14";
-          currentRoomPicture = "west/13~15";
-        }
-        else if(x < 56) {
-          _room = "洋館07";
-          currentRoomPicture = "west/07";
-        }
-      }
-    }
-    else {
-      _room = "not found";
-      currentRoomPicture = "none";
-    }
-    _tapPosition = pos;
-  }
+  String _selectedImage = '';
+  String _selectedPosition = '';
 
   @override
   Widget build(BuildContext context) {
-    final double imgWidth;
-    if (Image.asset(widget.imageUrl).width != null) {
-      imgWidth = (Image
-          .asset(widget.imageUrl)
-          .width! / 2);
-    }
-    else {
-      imgWidth = double.parse("140");
-    }
-    final double imgHeight;
-    if (Image.asset(widget.imageUrl).height != null) {
-      imgHeight = (Image
-          .asset(widget.imageUrl)
-          .height! / 2);
-    }
-    else {
-      imgHeight = double.parse("250");
-    }
-    return InteractiveViewer(
-      panEnabled: true,
-      boundaryMargin: EdgeInsets.only(left: imgWidth,right: imgWidth,top: imgHeight,bottom: imgHeight),
-      minScale: 0.5,
-      maxScale: widget.maxScale,
-      child: GestureDetector(
-          onTapDown:(details) => getPos(details),
-          child:
-          Column(
-            children: <Widget>[
-              Text("Offset: (${_tapPosition.dx.toStringAsFixed(2)},${_tapPosition.dy.toStringAsFixed(2)})"),
-              Transform.scale(
-                  scale: 4.0,
-                  child: SizedBox(
-                      width: 1450.0,
-                      height: imgHeight / 2 ,
-                      child: Image.asset(widget.imageUrl, fit: BoxFit.contain)
-                  )
-              ),
-              Text("current room:$_room"),
-              // Image(image: AssetImage('assets/$currentRoomPicture.jpg')),
-            ],
+    return GestureDetector(
+      onTapUp: _onTapUp,
+      child: Stack(
+        children: [
+          Image.asset(
+            'assets/map.png',
+            fit: BoxFit.cover,
           ),
-        )
+          Positioned(
+            top: 0,
+            left: 0,
+            child: Container(
+              width: 100,
+              height: 100,
+              color: Colors.transparent,
+            ),
+          ),
+          Positioned(
+            top: 0,
+            right: 0,
+            child: Container(
+              width: 100,
+              height: 100,
+              color: Colors.transparent,
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            child: Container(
+              width: 100,
+              height: 100,
+              color: Colors.transparent,
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            right: 0,
+            child: Container(
+              width: 100,
+              height: 100,
+              color: Colors.transparent,
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            child: Text(
+              '$_selectedPosition',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            right: 0,
+            child: Container(
+              width: 100,
+              height: 100,
+              child: _selectedImage.isEmpty
+                  ? Container()
+                  : Image.asset(
+                _selectedImage,
+                fit: BoxFit.cover,
+              ),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                border: Border.all(
+                  color: Colors.black,
+                  width: 1,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
+
+  void _onTapUp(TapUpDetails details) {
+    double x = details.localPosition.dx;
+    double y = details.localPosition.dy;
+
+    if (x <= 100 && y <= 100) {
+      setState(() {
+        _selectedPosition = 'Top Left';
+        _selectedImage = _imageMap['topLeft']!;
+      });
+    } else if (x > 100 && y <= 100) {
+      setState(() {
+        _selectedPosition = 'Top Right';
+        _selectedImage = _imageMap['topRight']!;
+      });
+    } else if (x <= 100 && y > 100) {
+      setState(() {
+        _selectedPosition = 'Bottom Left';
+        _selectedImage = _imageMap['bottomLeft']!;
+      });
+    } else if (x > 100 && y > 100) {
+      setState(() {
+        _selectedPosition = 'Bottom Right';
+        _selectedImage = _imageMap['bottomRight']!;
+      });
+    }
+  }
 }
+
 //==============================================================================
